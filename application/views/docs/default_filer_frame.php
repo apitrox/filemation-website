a141a0c6f1ffcc861d49e7f5731e9ee90f063c3d
apitrox <github@apitrox.com>
2025-10-20 19:29:45 -0500

    remove all

--- application/views/docs/default_filer_frame.php
+++
@@ -1,6 +0,0 @@
-<style>
-	body {
-		background-color: #FFFFFF !important;
-	}
-</style>
-<div style='margin-top: 50px; text-align: center;'><h3>No file has been selected.</h3><p>If files exist, select one from the <i>Files List</i> section located to the left.</p></div>
--- application/views/test/test_tooltips.php
+++
@@ -1,3 +0,0 @@
-<h1> Test Tooltips </h1>
-<button class="btn btn-default" title="This is the tooltip. This is a long tooltip">Test</button>
-<button class="btn btn-default" title="This is the tooltip">Test 2</button>
