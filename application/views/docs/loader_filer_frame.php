3eefac84a4af27a92acc7836239107aed3784ade
apitrox <github@apitrox.com>
2025-10-20 19:28:45 -0500

    remove all files

--- application/views/docs/loader_filer_frame.php
+++
@@ -1,8 +0,0 @@
-<style>
-	body {
-		background-color: #FFFFFF !important;
-	}
-</style>
-<div style='margin-top: 50px; text-align: center;'>
-	<img src="/assets/imgs/loader108.gif" />
-</div>
--- application/views/test/test_js_dates.php
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-	var date_value = "06.07.2014";
-	var date = Date.parse(date_value);
-	var new_date = date.toString('MM-dd-yy');
-	
-	document.write(new_date);
-</script>
